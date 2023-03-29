85e7f5a17ea8560abaaee6a23963189491d9ee1a
Choi MinJung <mj020503@naver.com>
2023-03-29 20:19:58 +0900

    두 정수의 덧셈_출력문추가

--- src/SumNum.java
+++ src/SumNum.java
@@ -4,10 +4,12 @@ public class SumNum{
         int a;
         int b;
         Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 정수를 입력하세요: ");
         a = sc.nextInt();
+        System.out.print("두 번째 정수를 입력하세요: ");
         b = sc.nextInt();
         int sum = a + b;
 
-        System.out.println(sum);
+        System.out.println(a + " + " + b + " = " + sum);
     }
 }
