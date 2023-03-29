8e9e72935c2fd7e84a03d95b2f0adef39ae533cf
Choi MinJung <mj020503@naver.com>
2023-03-29 20:15:55 +0900

    두 개의 정수 덧셈

---
+++ src/SumNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumNum{
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
