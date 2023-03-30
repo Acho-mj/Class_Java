6a548f085c177e3da1f6248d8e2350c455ff8a74
Choi MinJung <mj020503@naver.com>
2023-03-30 19:58:36 +0900

    args 배열 파라미터

---
+++ src/ArgsLength.java
@@ -0,0 +1,8 @@
+public class ArgsLength {
+    public static void main(String[] args){
+        for(String str : args)
+            System.out.println(str);
+        System.out.println("args.length= " + args.length);
+    }
+
+}
