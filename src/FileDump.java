d519223ab28778a64a92e889ecd8a1318863244c
Choi MinJung <mj020503@naver.com>
2023-06-08 20:28:26 +0900

    파일 내용 16진수로 출력

---
+++ src/FileDump.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class FileDump {
+    public static void main(String args[]){
+        if(args.length < 1){
+            System.out.println("java FileDump <filename>");
+            return;
+        }
+        FileInputStream in = null;
+        try{
+            in = new FileInputStream(args[0]);
+            byte arr[]= new byte[16];
+            while(true){
+                int num = in.read(arr);
+                if(num < 0)
+                    break;
+                for(int cnt = 0; cnt<num; cnt++)
+                    System.out.printf("%02X ", arr[cnt]);
+                System.out.println();
+            }
+        }catch(FileNotFoundException fnfe){
+            System.out.println(args[0] + "No File");
+        }catch(IOException ioe){
+            System.out.println(args[0] + "No Read");
+        }finally{
+            try{
+                in.close();
+            }catch(Exception e){
+
+            }
+        }
+    }
+}
